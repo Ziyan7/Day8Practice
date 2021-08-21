3e4239d7f14bd873ae7ed2e118a19f8d8a78c25b
Your Name <you@example.com>
2021-08-21 12:29:09 +0530

    Added: Program on Swapping Numbers

---
+++ Swap.java
@@ -0,0 +1,21 @@
+package Day8Practice;
+
+import java.util.Scanner;
+public class Swap {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		int num1=sc.nextInt();
+		System.out.println("Enter the second number:");
+		int num2=sc.nextInt();
+		System.out.println("Before swapping: "+num1+" "+num2);
+		sc.close();
+		int temp;		//Variable to store temporary value 
+		temp=num1;
+		num1=num2;
+		num2=temp;
+		System.out.println("After swapping: "+num1+" "+num2);
+	}
+
+}
